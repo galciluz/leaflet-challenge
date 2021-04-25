049c2965daf3dad3e87e55e0428994bda4f97a59
ALCILUZ GOMEZ <galciluz@gmail.com>
2021-04-24 20:17:38 -0400

    working on step 1

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,27 @@
+// Creating map object
+var myMap = L.map("map", {
+    center: [37.60, -95.6650],
+    zoom: 10
+  });
+  
+  // Adding tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
+  // Use this link to get the geojson data.
+  var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+  
+ 
+  // Grabbing our GeoJSON data..
+  d3.json(link).then(function(data) {
+    // Creating a geoJSON layer with the retrieved data
+    console.log (data);
+    }).addTo(myMap);
+  });
+  
